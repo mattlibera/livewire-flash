61b1acdc484287c7c5cbb9e2c62d042a21cc17a6
Mathis Haane <mathis.haane@placing-you.de>
2022-05-05 19:37:11 +0200

    fix/Alignment
    - fixed by a formatter

--- src/views/livewire/flash-message.blade.php
+++ src/views/livewire/flash-message.blade.php
@@ -1,35 +1,37 @@
 <div>
-    @if($shown)
-    <div class="rounded-r-md {{ $styles['bg-color'] }} p-4 border-l-4 {{ $styles['border-color'] }} mb-3">
-        <div class="flex">
-            @if ($iconset ?? false)
-                <div class="flex-shrink-0">
-                    <p class="{{ $styles['icon-color'] }}">
-                        @if($iconset === 'fa')
-                        <i class="{{ $styles['icon'][$iconset]['class'] }}"></i>
-                        @elseif($iconset === 'blade')
-                        @svg($styles['icon'][$iconset]['name'], $styles['icon'][$iconset]['class'])
-                        @endif
-                    </p>
+    @if ($shown)
+        <div class="{{ $styles['bg-color'] }} {{ $styles['border-color'] }} mb-3 rounded-r-md border-l-4 p-4">
+            <div class="flex">
+                @if ($iconset ?? false)
+                    <div class="flex-shrink-0">
+                        <p class="{{ $styles['icon-color'] }}">
+                            @if ($iconset === 'fa')
+                                <i class="{{ $styles['icon'][$iconset]['class'] }}"></i>
+                            @elseif($iconset === 'blade')
+                                @svg($styles['icon'][$iconset]['name'], $styles['icon'][$iconset]['class'])
+                            @endif
+                        </p>
+                    </div>
+                @endif
+                <div class="{{ $styles['text-color'] }} ml-3 text-sm font-medium leading-5">
+                    {!! $message['message'] !!}
                 </div>
-            @endif
-            <div class="ml-3 text-sm leading-5 font-medium {{ $styles['text-color'] }}">
-                {!! $message['message'] !!}
+                @if ($iconset && ($message['dismissable'] ?? false))
+                    <div class="ml-auto pl-3">
+                        <div class="-mx-1.5 -my-1.5">
+                            <button
+                                class="{{ $styles['text-color'] }} inline-flex rounded-md p-1.5 transition duration-150 ease-in-out focus:outline-none"
+                                wire:click="dismiss">
+                                @if ($iconset === 'fa')
+                                    <i class="{{ $dismiss[$iconset]['class'] }}"></i>
+                                @elseif($iconset === 'blade')
+                                    @svg($dismiss[$iconset]['name'], $dismiss[$iconset]['class'])
+                                @endif
+                            </button>
+                        </div>
+                    </div>
+                @endif
             </div>
-            @if ($iconset && ($message['dismissable'] ?? false))
-            <div class="ml-auto pl-3">
-                <div class="-mx-1.5 -my-1.5">
-                    <button class="inline-flex rounded-md p-1.5 {{ $styles['text-color'] }} focus:outline-none transition ease-in-out duration-150" wire:click="dismiss">
-                    @if($iconset === 'fa')
-                        <i class="{{ $dismiss[$iconset]['class'] }}"></i>
-                        @elseif($iconset === 'blade')
-                        @svg($dismiss[$iconset]['name'], $dismiss[$iconset]['class'])
-                        @endif
-                    </button>
-                </div>
-            </div>
-            @endif
         </div>
-    </div>
     @endif
 </div>
