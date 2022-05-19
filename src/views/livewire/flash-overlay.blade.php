20fe115d0d4fd4170dc52affb7f0c0f790ba24d7
Will Vincent <will@willvincent.com>
2022-05-18 21:02:49 -0500

    Update overlay template.

--- src/views/livewire/flash-overlay.blade.php
+++ src/views/livewire/flash-overlay.blade.php
@@ -1,20 +1,16 @@
-<div>
+<div x-data="{ open: @entangle('shown') }" x-cloak>
     @if($shown)
         <div class="fixed z-10 inset-0 overflow-y-auto">
             <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                <!--
-                  Background overlay, show/hide based on modal state.
-
-                  Entering: "ease-out duration-300"
-                    From: "opacity-0"
-                    To: "opacity-100"
-                  Leaving: "ease-in duration-200"
-                    From: "opacity-100"
-                    To: "opacity-0"
-                -->
-                <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-                    <div class="absolute inset-0 {{ $styles['overly-bg-color'] }} {{ $styles['overlay-bg-opacity'] }}"></div>
-                </div>
+                <div
+                    x-show="open"
+                    x-transition:enter="ease-out duration-300"
+                    x-transition:enter-start="opacity-0"
+                    x-transition:enter-end="opacity-100"
+                    x-transition:leave="ease-in duration-200"
+                    x-transition:leave-start="opacity-100"
+                    x-transition:leave-end="opacity-0" 
+                    class="fixed inset-0 bg-gray-500 transition-opacity" aria-hidden="true" />
 
                 <!-- This element is to trick the browser into centering the modal contents. -->
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
@@ -28,31 +24,40 @@
                     From: "opacity-100 translate-y-0 sm:scale-100"
                     To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 -->
-                <div class="inline-block relative align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                <div x-show="open"
+                    x-transition:enter="ease-out duration-300"
+                    x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                    x-transition:leave="ease-in duration-200"
+                    x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    class="inline-block relative align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                     <div class="sm:block absolute top-0 right-0 pt-4 pr-4">
                         <button wire:click="dismiss" type="button" class="text-gray-300 bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             <span class="sr-only">Close</span>
                             <!-- Heroicon name: x -->
-                            <i class="fas fa-times"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
                         </button>
                     </div>
                     <div>
                         <div class="mt-3 text-center sm:mt-5">
                             @if($message['title'])
-                                <h3 class="text-lg leading-6 font-medium {{ $styles['title-text-color'] }}" id="modal-headline">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
                                     {!! $message['title'] !!}
                                 </h3>
                             @endif
                             <div class="mt-2">
-                                <p class="text-sm {{ $styles['body-text-color'] }}">
+                                <p class="text-sm text-gray-500">
                                     {!! $message['message'] !!}
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div class="mt-5 sm:mt-6">
-                        <button wire:click="dismiss" type="button" class="inline-flex justify-center w-full rounded-md border {{ $styles['button-border-color'] }} shadow-sm px-4 py-2 {{ $styles['button-bg-color'] }} text-base font-medium {{ $styles['button-text-color'] }} {{ $styles['button-hover-bg-color'] }} {{ $styles['button-hover-text-color'] }} focus:outline-none focus:ring-2 focus:ring-offset-2 {{ $styles['button-focus-ring-color'] }} sm:text-sm {{ $styles['button-extra-classes'] }}">
-                            {{ $styles['button-text'] }}
+                        <button wire:click="dismiss" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 hover:text-indi focus:ring-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-sm">
+                            Close
                         </button>
                     </div>
                 </div>
